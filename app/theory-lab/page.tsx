b1dd5d3e1852a65acdfb437e51288fc236792bc8
soppkongen <syntaxloke@gmail.com>
2025-07-01 02:04:25 +0200

    Update page.tsx

--- app/theory-lab/page.tsx
+++ app/theory-lab/page.tsx
@@ -1,3 +1,7 @@
+
+'use client';
+
+
 import { useState, useRef, useEffect, useCallback } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
