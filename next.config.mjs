5dbbf3278a82503e1d51619e3032e6a901ce0a68
soppkongen <syntaxloke@gmail.com>
2025-07-04 01:08:31 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  env: {
+    WEAVIATE_URL: process.env.WEAVIATE_URL,
+    WEAVIATE_API_KEY: process.env.WEAVIATE_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
