82e9ff2b19754de64dfa6b5d770679e8b1fa4491
soppkongen <syntaxloke@gmail.com>
2025-07-04 01:31:22 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+ const nextConfig = {
   env: {
-    WEAVIATE_URL: process.env.WEAVIATE_URL,
-    WEAVIATE_API_KEY: process.env.WEAVIATE_API_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    WEAVIATE_URL: process.env.WEAVIATE_URL ?? 'MISSING',
+    WEAVIATE_API_KEY: process.env.WEAVIATE_API_KEY ?? 'MISSING',
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? 'MISSING',
   },
 };
 
