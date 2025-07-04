dfec53874412bb409ea5733ae5a3439fb72d52a1
soppkongen <syntaxloke@gmail.com>
2025-07-05 01:04:48 +0200

    Update document-processor.ts

--- lib/document-processor.ts
+++ lib/document-processor.ts
@@ -1,5 +1,3 @@
 export async function readFileAsText(file: File): Promise<string> {
-  const buffer = await file.arrayBuffer();
-  return new TextDecoder('utf-8').decode(buffer);
+  return await file.text();
 }
- 
